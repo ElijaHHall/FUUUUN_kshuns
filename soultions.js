2f779b5c732901c50890951ecfc1ceb77fdf4ac0
ElijahHHall <ehall851@gmail.com>
2018-03-23 08:01:25 -0600

    last commit

---
+++ soultions.js
@@ -0,0 +1,54 @@
+// // // // The difference between a parameter and an argument is the fact that parameters are temporary variables and arguments are the values contained within those variables.
+// // // // When a developer console.logs code, that code shows up in their console for them to see. Return actually puts the code on to a site, or a more solidified version.
+
+// // const checkPalindrome = (myString) => {
+
+// // 	for (i=0; i < myString.length; i++ ) {
+// // 		console.log(myString[i]);
+// // 	}
+
+
+// // 	return true;
+	
+// // 	console.log(myString);
+
+
+// // };
+
+// COMMIT 1
+// // checkPalindrome("radar");
+
+// const checkPalindrome = (word) => {
+// 	var lowerWord = word.toLowerCase();
+// 	if (lowerWord === lowerWord.split("").reverse().join("")) 
+// 	return true;
+// else
+// 	return false;
+		
+
+// }
+// checkPalindrome("Racecar");
+// COMMIT 2
+
+// const sumDigits = (num) => {
+// 	var output = [];
+// 	var sum = 0;
+// 	var sNumber =  num.toString().split('');
+// 	console.log(sNumber);
+// 	for (let i=0; i < sNumber.length; i++) {
+// 		sum += parseInt(sNumber[i]);
+		
+// 	}
+// 	console.log(sum);
+// }
+
+// sumDigits(42);
+// // Go thru each value of string
+// // turn each value into int
+// // push each value in num array
+
+// // numArr.push
+// COMMIT 3
+// I spent about 4 hours on these problems and I have tried attempting them.
+
+
